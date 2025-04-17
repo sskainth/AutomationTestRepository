d7a10cecae81da23f75b51e88456997d1b01d0e6
ssk <srivastavashikta@gmail.com>
2025-04-17 23:05:56 +0200

    set example added

---
+++ testInterviewPrep/src/main/java/javabasic/SetExample.java
@@ -0,0 +1,18 @@
+package javabasic;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SetExample {
+
+    public static void main(String[] args) {
+        Set <Integer> s = new HashSet<Integer>() ;
+            s.add(10);
+            s.add(90);
+            s.add(20);
+        System.out.println(s);
+        }
+    }
+
